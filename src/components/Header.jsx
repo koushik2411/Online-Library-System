1ae67c33a5af1ad4c1110e45f874a20939820e95
koushik2411 <koushikrk1324@gmail.com>
2026-06-04 10:02:23 +0530

    First commit. Created the basic folder structure and installed all dependencies.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <div>
+                <h1 className=" font-semibold text-xl border-b-2">
+                    ONLINE LIBRARY SYSTEM
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/data/bookData.js
@@ -0,0 +1,26 @@
+const bookData = [
+    {
+        id: 1,
+        title: "",
+        image: "",
+        category: "",
+        author: "",
+        publishYear: "",
+        description: "",
+        rating: ,
+    },
+]
+
+export default bookData;
+
+// TEMPLATE
+// {
+//     id: ,
+//     title: "",
+//     image: "",
+//     category: "",
+//     author: "",
+//     publishYear: "",
+//     description: "",
+//     rating: ,
+// },
